f45d841d962c3aad85a3619ca89a2baa70e0182b
Valeriy Kutuzov <Kutuzov.V.A@sberbank.ru>
2020-03-24 12:42:03 +0400

    Initial project

---
+++ NetworkLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by Valeriy Kutuzov on 24.03.2020.
+//  Copyright © 2020 Valeriy Kutuzov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
